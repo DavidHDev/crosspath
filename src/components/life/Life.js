f65295be4534624a2ff64218f5408bf01ad44191
David <haz.d@outlook.com>
2021-12-08 00:36:01 +0200

    added compressed images

--- src/components/life/Life.js
+++ src/components/life/Life.js
@@ -1,6 +1,12 @@
 import { Image } from 'antd';
 import './life.scss';
 import { Fade } from 'react-reveal';
+import img1 from '../../assets/img/img1.jpeg';
+import img2 from '../../assets/img/img2.jpeg';
+import img3 from '../../assets/img/img3.jpeg';
+import img4 from '../../assets/img/img4.jpeg';
+import img5 from '../../assets/img/img5.jpeg';
+import img6 from '../../assets/img/img6.jpeg';
 
 const Life = () => {
     return (
@@ -10,12 +16,12 @@ const Life = () => {
                     <h2 className='life-title'>Explore Life @ Crosspath</h2>
                     <div className='gallery'>
                         <Image.PreviewGroup>
-                            <Image loading='lazy' placeholder={true} className='g-image' src="https://images.unsplash.com/photo-1594732832278-abd644401426?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" />
-                            <Image loading='lazy' placeholder={true} className='g-image' src="https://images.unsplash.com/photo-1558403299-52a71df71bcf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" />
-                            <Image loading='lazy' placeholder={true} className='g-image' src="https://images.unsplash.com/photo-1517048676732-d65bc937f952?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" />
-                            <Image loading='lazy' placeholder={true} className='g-image' src="https://images.unsplash.com/photo-1516062423079-7ca13cdc7f5a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2366&q=80" />
-                            <Image loading='lazy' placeholder={true} className='g-image' src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" />
-                            <Image loading='lazy' placeholder={true} className='g-image' src="https://images.unsplash.com/photo-1507209696998-3c532be9b2b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" />
+                            <Image loading='lazy' placeholder={true} className='g-image' src={img1} />
+                            <Image loading='lazy' placeholder={true} className='g-image' src={img2}/>
+                            <Image loading='lazy' placeholder={true} className='g-image' src={img3} />
+                            <Image loading='lazy' placeholder={true} className='g-image' src={img4} />
+                            <Image loading='lazy' placeholder={true} className='g-image' src={img5} />
+                            <Image loading='lazy' placeholder={true} className='g-image' src={img6} />
                         </Image.PreviewGroup>
                     </div>
                 </div>
